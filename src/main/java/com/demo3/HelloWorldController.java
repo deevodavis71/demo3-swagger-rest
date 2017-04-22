fac29c90f2ee9eb20c671059378acec7da2d6e4c
Steve Davis <davissa@uk.innovation-group.com>
2017-04-22 20:21:06 +0100

    In branch 4

--- src/main/java/com/demo3/HelloWorldController.java
+++ src/main/java/com/demo3/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.demo3;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by stevedavis on 22/04/2017. OK - Another change
  */
 @RestController
-@RequestMapping ("/api")
+@RequestMapping("/api")
 public class HelloWorldController {
 
-    @RequestMapping (method = RequestMethod.GET, value = "/hello")
-    public Greeting helloWorld ()
-    {
+    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+    public Greeting helloWorld() {
         Greeting g = new Greeting();
         g.setGreeting("Hello World greeting from Branch 4!");
 
