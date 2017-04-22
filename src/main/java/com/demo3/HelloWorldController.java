71df1a233aa863860ef6a3ac49b71e3dda70700e
Steve Davis <davissa@uk.innovation-group.com>
2017-04-22 19:42:46 +0100

    Initial Commit (THIRD)

---
+++ src/main/java/com/demo3/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.demo3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by stevedavis on 22/04/2017. OK
+ */
+@RestController
+@RequestMapping ("/api")
+public class HelloWorldController {
+
+    @RequestMapping (method = RequestMethod.GET, value = "/hello")
+    public Greeting helloWorld ()
+    {
+        Greeting g = new Greeting();
+        g.setGreeting("Hello greeting!");
+
+        return g;
+    }
+}
