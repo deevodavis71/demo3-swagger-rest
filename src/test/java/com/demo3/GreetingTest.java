eac729da71854d399383589fb49156a55a20fa16
Steve Davis <davissa@uk.innovation-group.com>
2017-04-22 20:25:34 +0100

    Tests

---
+++ src/test/java/com/demo3/GreetingTest.java
@@ -0,0 +1,21 @@
+package com.demo3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stevedavis on 22/04/2017.
+ */
+public class GreetingTest {
+    @Test
+    public void getGreeting() throws Exception {
+
+    }
+
+    @Test
+    public void setGreeting() throws Exception {
+
+    }
+
+}
