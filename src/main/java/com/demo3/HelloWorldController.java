ef421b92217829aaa55d9c8514043167f12f2d82
Steve Davis <deevodavis71@gmail.com>
2017-06-04 08:34:08 +0100

    Changed formatter

--- src/main/java/com/demo3/HelloWorldController.java
+++ src/main/java/com/demo3/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.demo3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,34 +11,45 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by stevedavis on 22/04/2017. OK - Another change
  */
 @RestController
-@RequestMapping ("/api")
+@RequestMapping("/api")
 public class HelloWorldController
 {
-    @RequestMapping (method = RequestMethod.GET, value = "/hello")
-    public Greeting helloWorld ()
-    {
-        Greeting g = new Greeting ();
-        g.setGreeting ("Hello World greeting from Branch 4!");
+	@RequestMapping(method = RequestMethod.GET, value = "/hello")
+	public Greeting helloWorld ()
+	{
+		Greeting g = new Greeting();
+		g.setGreeting("Hello World greeting from Branch 4!");
 
-        // Some new changes
+		// Some new changes
 
-        return g;
-    }
+		return g;
+	}
 
-    @RequestMapping (method = RequestMethod.GET, value = "/helloUniverse")
-    public Greeting helloUniverse ()
-    {
-        Greeting g = new Greeting ();
-        g.setGreeting ("Hello Universe greeting from Branch 4!");
+	@RequestMapping(method = RequestMethod.GET, value = "/helloUniverse")
+	public Greeting helloUniverse ()
+	{
+		Greeting g = new Greeting();
+		g.setGreeting("Hello Universe greeting from Branch 4!");
 
-        // Some new changes
+		// Some new changes
 
-        return g;
-    }
-    
-    @RequestMapping (method = RequestMethod.GET, value = "/age")
-    public int getAge ()
-    {
-    	return 42;
-    }
+		return g;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/age")
+	public int getAge ()
+	{
+		List<String> names = new ArrayList<>();
+		names.add("Steve");
+		names.add("Karen");
+		names.add("Adam");
+		names.parallelStream().forEach(System.out::println);
+
+		return getDefaultAge();
+	}
+
+	private int getDefaultAge ()
+	{
+		return 42;
+	}
 }
